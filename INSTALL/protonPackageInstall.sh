59919bf75d08ce33c6bc5846bb0cfe38347cf9cd
Rajeev Srivastava <rajeev.srivastava@gmail.com>
2026-09-10 16:19:24 -0400

    Add the package install scripts, and say electron at the prompt
    centos_package_install.sh, rhel_package_install.sh, protonPackageInstall.sh and
    localPerlModules.txt from potato's INSTALL, completing that directory.  Nothing
    in the container files references them, so they stand on their own as host
    setup.
    
    $PROMPT is now electron_shell in all three tool lists.  It was proton_shell in
    the hier and proto lists that came from potato and, oddly, tool_shell in this
    repo's own -- so the main tool never said proton in the first place; all three
    now agree.
    
    Only the prompt is changed.  These strings still say proton and are left for a
    separate decision, since they are wording rather than identity: the --version
    help line, the startup timing line, the daemon message and the usage banner in
    the hier and proto lists.  PROTON_HOME, dbfGlobalGetProtonHome and the proton
    executable names are untouched on purpose -- they are paths and file names, not
    prompts, and renaming them is a different job.

---
+++ INSTALL/protonPackageInstall.sh
@@ -0,0 +1,61 @@
+
+#sudo apt-get install gperf
+#sudo apt-get install flex
+#sudo apt-get install bison
+#
+## --------- update --------#
+#wget  https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/liberty-parser/gen-liberty.so.tar.gz
+#wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/liberty-parser/liberty_parse-2.5e.tar.gz
+#tar -zxvf liberty_parse-2.5e.tar.gz 
+#tar -zxvf gen-liberty.so.tar.gz 
+#mv gen-liberty.so liberty_parse-2.5
+#cd liberty_parse-2.5/
+#./configure 
+#make
+#gen-liberty.so/
+#make
+#sudo cp /home/ubuntu/Projects/potato/liberty_parse-2.5/perl/liberty.pm /usr/local/lib/perl/5.18.2/
+#sudo cp /home/ubuntu/Projects/potato/liberty_parse-2.5/gen-liberty.so/liberty.so  /usr/local/lib/perl/5.18.2/
+
+
+sudo apt-get install perl-tk
+cpanm Tk::WorldCanvas
+cpanm DBI
+sudo apt-get install libdbd-mysql-perl
+cpanm Spreadsheet::Read
+cpanm Spreadsheet::WriteExcel
+cpanm Text::CSV_XS
+cpanm Tk::DynaTabFrame
+cpanm File::Data
+sudo cp /home/ubuntu/perl5/lib/perl5/File/File/Data.pm /home/ubuntu/perl5/lib/perl5/File/Data.pm
+sudo apt-get install libxml-sax-expat-perl
+cpanm XML::Simple
+cpanm PDF::Create
+cpanm Verilog::Netlist
+cpanm Bit::Vector
+cpanm Verilog::VCD
+cpanm GDS2
+cpanm Frontier::Daemon
+sudo apt-get install libgd-perl
+cpanm Graphics::ColorNames
+cpanm Spreadsheet::ParseExcel
+cpanm XML::Writer
+cpanm Switch
+sudo apt-get install libtk-tablematrix-perl
+cpanm Tk::ProgressBar::Mac
+
+mkdir  /home/ubuntu/perl5/lib/perl5/Local
+cp LIBS/TeeOutput.pm /home/ubuntu/perl5/lib/perl5/Local/
+
+
+cpanm Proc::Simple
+cpanm Proc::ProcessTable
+cpanm Tk::Splashscreen
+cpanm File::ReadBackwards
+cpanm XML::Excel
+cpanm Graph::Directed
+cpanm Math::Polygon
+cpanm Git::Repository
+
+cpanm Math::Clipper
+cpanm Spreadsheet::XLSX
---
+++ INSTALL/rhel_package_install.sh
@@ -0,0 +1,59 @@
+
+
+wget http://cpan.metacpan.org/authors/id/A/AP/APEIRON/local-lib-1.008004.tar.gz
+tar xzf local-lib-1.008004.tar.gz
+cd local-lib-1.008004
+perl Makefile.PL --bootstrap
+echo 'eval $(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)' >> ~/.bashrc
+source ~/.bashrc
+
+echo $PERL_LOCAL_LIB_ROOT
+setenv PERL_LOCAL_LIB_ROOT "/home/rsrivastava/perl5"
+setenv PERL_MB_OPT "--install_base /home/rsrivastava/perl5"
+setenv PERL_MM_OPT "INSTALL_BASE=/home/rsrivastava/perl5"
+setenv PERL5LIB "/home/rsrivastava/perl5/lib/perl5/x86_64-linux-thread-multi:/home/rsrivastava/perl5/lib/perl5"
+setenv PATH "/home/rsrivastava/perl5/bin:$PATH"
+
+
+cpanm -f Tk
+cpanm Tk::WorldCanvas
+cpanm Tk::DynaTabFrame
+cpanm Spreadsheet::Read
+cpanm Spreadsheet::WriteExcel
+cpanm Text::CSV_XS
+cpanm File::Data
+cp /home/rsrivastava/perl5/lib/perl5/File/File/Data.pm /home/rsrivastava/perl5/lib/perl5/File/
+
+cpanm XML::Simple
+cpanm PDF::Create
+cpanm Verilog::Netlist
+cpanm Verilog::VCD
+cpanm GDS2
+cpanm Frontier::Daemon
+cpanm Graphics::ColorNames
+mkdir  ~/perl5/lib/perl5/Local
+
+cpanm Spreadsheet::ParseExcel
+cpanm XML::Writer
+cpanm Switch
+cpanm Tk::ProgressBar::Mac
+
+mkdir ~/perl5/lib/perl5/Local/
+cp ~/potato/LIBS/TeeOutput.pm ~/perl5/lib/perl5/Local/
+
+cpanm Proc::Simple
+cpanm Proc::ProcessTable
+cpanm Tk::Splashscreen
+cpanm File::ReadBackwards
+cpanm XML::Excel
+cpanm Graph::Directed
+cpanm Math::Polygon
+cpanm Git::Repository
+cpanm Math::Clipper
+cpanm Mojo::UserAgent
+cpanm Tk::TableMatrix::Spreadsheet
+
+setenv PROTON_HOME /home/rsrivastava/potato/
+
+cpanm List::Part
+cpanm List::Flatten
